82d39037f236ccfc1915c539d857b35eb41e2733
Kosta <kostastoupas@gmail.com>
2018-10-15 22:51:16 +0200

    Removed the .idea folder

---
+++ lib/data/contributors.dart
@@ -0,0 +1,41 @@
+class Contributors {
+  List<Contributor> contributors;
+
+  Contributors({this.contributors});
+
+  Contributors.fromJson(Map<String, dynamic> json) {
+    if (json['contributors'] != null) {
+      contributors = new List<Contributor>();
+      json['contributors'].forEach((v) {
+        contributors.add(new Contributor.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this.contributors != null) {
+      data['contributors'] = this.contributors.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class Contributor {
+  String name;
+  String profile;
+
+  Contributor({this.name, this.profile});
+
+  Contributor.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+    profile = json['profile'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['name'] = this.name;
+    data['profile'] = this.profile;
+    return data;
+  }
+}
