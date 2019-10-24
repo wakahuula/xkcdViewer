b6e3d2a6260e99df507532771c027fa74ecd9aad
ahmdaeyz <ahmedalarabe5@gmail.com>
2019-10-24 23:04:28 +0200

    Add drag comic up to show alt text

--- lib/widgets/comic_view.dart
+++ lib/widgets/comic_view.dart
@@ -6,56 +6,98 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:xkcd/data/comic.dart';
 import 'package:xkcd/utils/preferences.dart';
 
-class ComicView extends StatelessWidget {
+class ComicView extends StatefulWidget {
   final Comic comic;
-  final SharedPreferences _prefs = Preferences.prefs;
-
-  // props to https://github.com/T-Rex96/Easy_xkcd/blob/23ade11a2bcb520e5d3868f8368050413db08ed3/app/src/main/java/de/tap/easy_xkcd/utils/Comic.java
-  final no2xVersion = [1193, 1446, 1667, 1735, 1739, 1744, 1778];
-
   ComicView(this.comic);
 
   @override
+  _ComicViewState createState() => _ComicViewState();
+}
+
+class _ComicViewState extends State<ComicView>
+    with SingleTickerProviderStateMixin {
+  final SharedPreferences _prefs = Preferences.prefs;
+
+  final no2xVersion = [1193, 1446, 1667, 1735, 1739, 1744, 1778];
+  Animation<Offset> slideup;
+  AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 500));
+    slideup = Tween<Offset>(begin: Offset(0, 0), end: Offset(0, -0.22)).animate(
+        CurvedAnimation(curve: Curves.easeInOutQuad, parent: controller));
+    slideup.addListener(() {
+      setState(() {});
+    });
+  }
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+  @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onLongPress: () {
-        _showAltDialog(context);
-      },
-      child: Container(
-        padding: const EdgeInsets.only(top: 0, right: 16, bottom: 32, left: 16),
-        child: Hero(
-          tag: 'hero-${comic.num}',
-          child: FadeIn(
-            child: PhotoView(
-              maxScale: PhotoViewComputedScale.covered,
-              minScale: PhotoViewComputedScale.contained,
-              backgroundDecoration: BoxDecoration(
-                color: Color(Theme.of(context).primaryColor.value),
+    return Stack(
+      children: <Widget>[
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            padding:
+                const EdgeInsets.only(bottom: 30, right: 20, left: 20, top: 20),
+            child: Text(widget.comic.alt),
+          ),
+        ),
+        GestureDetector(
+          onTapDown: (details) {
+            controller.reverse();
+          },
+          onVerticalDragStart: (dragdetails) {
+            controller.forward();
+          },
+          child: SlideTransition(
+            position: slideup,
+            child: Material(
+              elevation: 4,
+              child: Hero(
+                tag: 'hero-${widget.comic.num}',
+                child: FadeIn(
+                  child: PhotoView(
+                    maxScale: PhotoViewComputedScale.covered,
+                    minScale: PhotoViewComputedScale.contained,
+                    backgroundDecoration: BoxDecoration(
+                      color: Color(Theme.of(context).primaryColor.value),
+                    ),
+                    loadingChild: Container(
+                        child: Center(child: CircularProgressIndicator())),
+                    imageProvider: CachedNetworkImageProvider(_getImageUrl()),
+                  ),
+                ),
               ),
-              loadingChild: Center(child: CircularProgressIndicator()),
-              imageProvider: CachedNetworkImageProvider(_getImageUrl()),
             ),
           ),
         ),
-      ),
+      ],
     );
   }
 
   String _getImageUrl() {
-    final num = comic.num;
+    final num = widget.comic.num;
     final dataSaver = _prefs.getBool('data_saver') ?? false;
     if (dataSaver || no2xVersion.contains(num)) {
-      return comic.img;
+      return widget.comic.img;
     }
     if (num >= 1084) {
-      var img = comic.img;
+      var img = widget.comic.img;
       return img.substring(0, img.lastIndexOf('.')) + "_2x.png";
     }
-    return comic.img;
+    return widget.comic.img;
   }
 
   void _showAltDialog(BuildContext context) {
-    if (comic.alt.isEmpty) {
+    if (widget.comic.alt.isEmpty) {
       return;
     }
     _vibrate();
@@ -68,11 +110,11 @@ class ComicView extends StatelessWidget {
           contentPadding: const EdgeInsets.all(0),
           title: Container(
             padding: const EdgeInsets.all(16),
-            child: Text('${comic.num}: ${comic.title}'),
+            child: Text('${widget.comic.num}: ${widget.comic.title}'),
           ),
           content: Container(
             padding: const EdgeInsets.all(20),
-            child: Text(comic.alt),
+            child: Text(widget.comic.alt),
           ),
         );
       },
