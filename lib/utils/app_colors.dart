5f469be0d57b0d1cf29fe38f22e8255f9f111549
Britannio <britanniojarrett@gmail.com>
2019-11-12 20:42:36 +0000

    add surface color variable to app colors remove safeArea from favorites page

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class AppColors {
   static ThemeData theme({@required Color accent, @required bool dark}) {
     final Brightness brightness = dark ? Brightness.dark : Brightness.light;
+    final Color surfaceColor = dark ? const Color(0xFF121212) : Colors.white;
     return ThemeData(
       brightness: brightness,
       fontFamily: 'FiraMono',
@@ -12,15 +13,19 @@ class AppColors {
           TargetPlatform.android: OpenUpwardsPageTransitionsBuilder(),
         },
       ),
-      canvasColor: dark ? const Color(0xFF121212) : Colors.white,
-      primaryColor: dark ? const Color(0xFF121212) : Colors.white,
-      primaryColorLight: dark ? const Color(0xFF121212) : Colors.white,
-      primaryColorDark: dark ? const Color(0xFF121212) : Colors.white,
-      bottomAppBarColor: dark ? const Color(0xFF121212) : Colors.white,
+      canvasColor: surfaceColor,
+      primaryColor: surfaceColor,
+      primaryColorLight: surfaceColor,
+      primaryColorDark: surfaceColor,
+      bottomAppBarColor: surfaceColor,
       primarySwatch: Colors.deepPurple,
       accentColor: accent,
       dividerColor: dark ? Color(0xFF2E2E31) : Colors.black12,
-      appBarTheme: AppBarTheme(brightness: brightness, elevation: 0),
+      appBarTheme: AppBarTheme(
+        brightness: brightness,
+        elevation: 0,
+        color: surfaceColor,
+      ),
     );
   }
 }
